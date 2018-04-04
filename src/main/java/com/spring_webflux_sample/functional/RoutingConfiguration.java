09c86aa7e4335752e996d9261e1e54e03888231b
inhochoi <revaer@naver.com>
2018-04-04 22:45:19 +0900

    Add example webflux hello controller

--- src/main/java/com/spring_webflux_sample/functional/RoutingConfiguration.java
+++ src/main/java/com/spring_webflux_sample/functional/RoutingConfiguration.java
@@ -4,14 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
 
+import static org.springframework.http.MediaType.ALL;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
 public class RoutingConfiguration {
     @Bean
     public RouterFunction<ServerResponse> routerFunction() {
-        return route(GET("/"), request -> ServerResponse.ok().syncBody("Hello World"));
+        return route(GET("/hello").and(accept(ALL)), request -> ServerResponse.ok().body(Mono.just("Hello World"), String.class));
     }
 }
