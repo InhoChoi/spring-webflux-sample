3e252fa35995b2a8ce650393bf36d4353a1034c9
root <root@DESKTOP-CI2PEDC.localdomain>
2018-04-04 22:16:54 +0900

    Add controller mono example

--- src/main/java/com.spring-webflux-sample/HelloController.java
+++ src/main/java/com.spring-webflux-sample/HelloController.java
@@ -1,18 +1,22 @@
 package hello;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import reactor.core.publisher.Mono;
 
-@Controller
+@RestController
+@EnableWebFlux
 @EnableAutoConfiguration
 public class HelloController {
 
     @RequestMapping("/")
     @ResponseBody
-    String home() {
-        return "Hello World!";
+    Mono<String> home() {
+        return Mono.just("Hello World");
     }
 
     public static void main(String[] args) {
